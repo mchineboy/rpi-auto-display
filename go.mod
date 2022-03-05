89592c2348aac7e69fa20a9476673ca19dcfdbdf
Tyler Hardison <tyler@stninc.com>
2022-03-05 10:00:22 -0800

    test runner

---
+++ go.mod
@@ -0,0 +1,11 @@
+module github.com/mchineboy/rpi-auto-display
+
+go 1.17
+
+require (
+	github.com/fogleman/gg v1.3.0 // indirect
+	github.com/golang/freetype v0.0.0-20170609003504-e2365dfdc4a0 // indirect
+	github.com/stianeikeland/go-rpio/v4 v4.6.0 // indirect
+	go.riyazali.net/epd v0.0.0-20200921164209-84f4fd4927b9 // indirect
+	golang.org/x/image v0.0.0-20200921011436-3a743ba83854 // indirect
+)
---
+++ src/interface/main.go
@@ -0,0 +1,42 @@
+package auto_interface
+
+import (
+	"log"
+
+	"github.com/stianeikeland/go-rpio/v4"
+	"go.riyazali.net/epd"
+)
+
+type AutoInterface struct {
+	Display *epd.EPD
+}
+
+type ReadablePinPatch struct{ rpio.Pin }
+
+func (pin ReadablePinPatch) Read() uint8 { return uint8(pin.Pin.Read()) }
+
+func init() {
+	//start the GPIO controller
+	if err := rpio.Open(); err != nil {
+		log.Fatalf("[FATAL] failed to start gpio: %v", err)
+	}
+
+	// Enable SPI on SPI0
+	if err := rpio.SpiBegin(rpio.Spi0); err != nil {
+		log.Fatalf("[FATAL] failed to enable SPI: %v", err)
+	}
+
+	// configure SPI settings
+	rpio.SpiSpeed(4_000_000)
+	rpio.SpiMode(0, 0)
+
+	rpio.Pin(17).Mode(rpio.Output)
+	rpio.Pin(25).Mode(rpio.Output)
+	rpio.Pin(8).Mode(rpio.Output)
+	rpio.Pin(24).Mode(rpio.Input)
+}
+
+func New() *AutoInterface {
+	AutoInt := &AutoInterface{Display: epd.New(rpio.Pin(17), rpio.Pin(25), rpio.Pin(8), ReadablePinPatch{rpio.Pin(24)}, rpio.SpiTransmit)}
+	return AutoInt
+}
