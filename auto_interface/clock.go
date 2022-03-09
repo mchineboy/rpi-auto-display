494c26fe905cad384b5fa4914660225fca9e2578
Tyler Hardison <tyler@stninc.com>
2022-03-09 12:49:13 -0800

    ..

--- auto_interface/clock.go
+++ auto_interface/clock.go
@@ -28,7 +28,7 @@ func (AutoInt *AutoInterface) DrawClock() {
 	wtime, htime := AutoInt.Screen.MeasureString(time)
 	log.Printf("width %f, height %f", wtime, htime)
 	AutoInt.Screen.Rotate(gg.Radians(90))
-	log.Printf("x: %f, y: %f\n", float64(AutoInt.Display.Height/2), float64(AutoInt.Display.Width/2))
+	log.Printf("x: %f, y: %f\n", float64(AutoInt.Display.Width/2)-wtime, float64(AutoInt.Display.Height/2)-14)
 	AutoInt.Screen.DrawStringAnchored(time, float64(AutoInt.Display.Width/2)-wtime, float64(AutoInt.Display.Height/2)-14, 1.0, 1.0)
 	AutoInt.Screen.Stroke()
 	log.Printf("Update clock: %s\n", time)
