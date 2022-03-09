adcf87d47c77d56feb67fa9da6a70327029d08d6
Tyler Hardison <tyler@stninc.com>
2022-03-08 16:55:02 -0800

    ..

--- auto_interface/clock.go
+++ auto_interface/clock.go
@@ -10,6 +10,6 @@ func (AutoInt *AutoInterface) DrawClock() {
 	time := currentTime.Format("Mon Jan 2 15:04:05")
 	AutoInt.Screen.SetColor(color.Black)
 	htime, wtime := AutoInt.Screen.MeasureString(time)
-	AutoInt.Screen.DrawString(time, htime-float64(AutoInt.Display.Height), wtime-float64(AutoInt.Display.Width))
+	AutoInt.Screen.DrawString(time, float64(AutoInt.Display.Height)-htime, float64(AutoInt.Display.Width)-wtime)
 	AutoInt.Screen.Stroke()
 }
