f0d72ac9691a5fc30e6620224d1237aed37870b0
Tyler Hardison <tyler@stninc.com>
2022-03-09 14:43:57 -0800

    maybe

--- auto_interface/clock.go
+++ auto_interface/clock.go
@@ -6,17 +6,32 @@ import (
 	"time"
 
 	"github.com/fogleman/gg"
+	"github.com/golang/freetype/truetype"
+	"golang.org/x/image/font/gofont/goregular"
 )
 
 func (AutoInt *AutoInterface) DrawClock() {
 	currentTime := time.Now()
 	time := currentTime.Format("Mon, Jan 2 15:04")
 	AutoInt.Screen.SetColor(color.Black)
+
+	font, err := truetype.Parse(goregular.TTF)
+
+	if err != nil {
+		panic(err)
+	}
+
+	face := truetype.NewFace(font, &truetype.Options{
+		Size: 10,
+	})
+	AutoInt.Screen.SetFontFace(face)
+
 	wtime, htime := AutoInt.Screen.MeasureString(time)
+
 	log.Printf("width %f, height %f", wtime, htime)
 	AutoInt.Screen.Rotate(gg.Radians(90))
-	log.Printf("x: %f, y: %f\n", float64(currentTime.Second()), float64((AutoInt.Display.Width-14)*-1))
-	AutoInt.Screen.DrawString(time, float64(currentTime.Second()), float64((AutoInt.Display.Width-14)*-1))
+	log.Printf("x: %f, y: %f\n", float64(AutoInt.Display.Width)-wtime, float64((AutoInt.Display.Width-14)*-1))
+	AutoInt.Screen.DrawString(time, float64(AutoInt.Display.Width)-wtime, float64((AutoInt.Display.Width-14)*-1))
 	AutoInt.Screen.Stroke()
 	log.Printf("Update clock: %s\n", time)
 }
--- auto_interface/drawx.go
+++ auto_interface/drawx.go
@@ -4,6 +4,7 @@ import "image/color"
 
 func (AutoInt *AutoInterface) DrawX() {
 	AutoInt.Screen.SetColor(color.Black)
-	AutoInt.Screen.DrawLine(0, 0, float64(AutoInt.Display.Width), float64(AutoInt.Display.Height*-1))
+	AutoInt.Screen.SetLineWidth(1.0)
+	AutoInt.Screen.DrawLine(0, float64((AutoInt.Display.Height-14)*-1), float64(AutoInt.Display.Width), float64((AutoInt.Display.Height-14)*-1))
 	AutoInt.Screen.Stroke()
 }
