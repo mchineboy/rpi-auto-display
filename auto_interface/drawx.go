4ea223ba3d3b6e6c8e55026c442accc3612c3e15
Tyler Hardison <tyler@stninc.com>
2022-03-09 15:19:57 -0800

    ..

--- auto_interface/drawx.go
+++ auto_interface/drawx.go
@@ -1,8 +1,11 @@
 package auto_interface
 
+import "log"
+
 func (AutoInt *AutoInterface) DrawX() {
 	AutoInt.Screen.SetRGBA(0, 0, 0, 1)
 	AutoInt.Screen.SetLineWidth(1.0)
 	AutoInt.Screen.DrawLine(0, float64((AutoInt.Display.Height-14)*-1), float64(AutoInt.Display.Width), float64((AutoInt.Display.Height-14)*-1))
+	log.Printf("%f %f %f %f\n", 0.0, float64((AutoInt.Display.Height-14)*-1), float64(AutoInt.Display.Width), float64((AutoInt.Display.Height-14)*-1))
 	AutoInt.Screen.Stroke()
 }
