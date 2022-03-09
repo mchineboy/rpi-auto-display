5f122723285b2f1f123141ac9e52f89f990a8367
Tyler Hardison <tyler@stninc.com>
2022-03-09 14:56:38 -0800

    ..

--- auto_interface/drawx.go
+++ auto_interface/drawx.go
@@ -1,10 +1,8 @@
 package auto_interface
 
-import "image/color"
-
 func (AutoInt *AutoInterface) DrawX() {
-	AutoInt.Screen.SetColor(color.Black)
+	AutoInt.Screen.SetRGBA(1, 1, 1, 1)
 	AutoInt.Screen.SetLineWidth(1.0)
-	AutoInt.Screen.DrawLine(0, float64((AutoInt.Display.Height-14)*-1), float64(AutoInt.Display.Width), float64((AutoInt.Display.Height-14)*-1))
+	AutoInt.Screen.DrawLine(0, float64((AutoInt.Display.Height - 14)), float64(AutoInt.Display.Width), float64((AutoInt.Display.Height - 14)))
 	AutoInt.Screen.Stroke()
 }
