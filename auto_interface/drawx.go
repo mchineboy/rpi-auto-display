7317717c4671238859f24febe9437e7131565410
Tyler Hardison <tyler@stninc.com>
2022-03-09 14:30:43 -0800

    maybe?

--- auto_interface/drawx.go
+++ auto_interface/drawx.go
@@ -4,6 +4,6 @@ import "image/color"
 
 func (AutoInt *AutoInterface) DrawX() {
 	AutoInt.Screen.SetColor(color.Black)
-	AutoInt.Screen.DrawLine(0, 0, float64(AutoInt.Display.Width), float64(AutoInt.Display.Height))
+	AutoInt.Screen.DrawLine(0, 0, float64(AutoInt.Display.Width), float64(AutoInt.Display.Height*-1))
 	AutoInt.Screen.Stroke()
 }
