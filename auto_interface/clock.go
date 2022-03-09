f525dd2a6a71967ef9e89f4385cbfe2a1c12fd67
Tyler Hardison <tyler@stninc.com>
2022-03-09 12:08:00 -0800

    come on!

--- auto_interface/clock.go
+++ auto_interface/clock.go
@@ -6,15 +6,28 @@ import (
 	"time"
 
 	"github.com/fogleman/gg"
+	"github.com/golang/freetype/truetype"
+	"golang.org/x/image/font/gofont/goregular"
 )
 
 func (AutoInt *AutoInterface) DrawClock() {
 	currentTime := time.Now()
 	time := currentTime.Format("Mon, Jan 2 15:04")
 	AutoInt.Screen.SetColor(color.Black)
+
+	font, err := truetype.Parse(goregular.TTF)
+
+	if err != nil {
+		panic(err)
+	}
+
+	face := truetype.NewFace(font, &truetype.Options{
+		Size: 10,
+	})
+	AutoInt.Screen.SetFontFace(face)
 	wtime, htime := AutoInt.Screen.MeasureString(time)
 	log.Printf("width %f, height %f", wtime, htime)
-	AutoInt.Screen.Rotate(gg.Radians(180))
+	AutoInt.Screen.Rotate(gg.Radians(90))
 	log.Printf("x: %f, y: %f\n", float64(AutoInt.Display.Height/2), float64(AutoInt.Display.Width/2))
 	AutoInt.Screen.DrawStringAnchored(time, float64(AutoInt.Display.Width/2)-wtime, float64(AutoInt.Display.Height/2)-14, 0.5, 0.5)
 	AutoInt.Screen.Stroke()
