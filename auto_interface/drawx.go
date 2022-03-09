15e461b6564cde3b94e134824e3b106af9a96019
Tyler Hardison <tyler@stninc.com>
2022-03-09 15:18:48 -0800

    ..

--- auto_interface/drawx.go
+++ auto_interface/drawx.go
@@ -1,7 +1,7 @@
 package auto_interface
 
 func (AutoInt *AutoInterface) DrawX() {
-	AutoInt.Screen.SetRGBA(1, 1, 1, 1)
+	AutoInt.Screen.SetRGBA(0, 0, 0, 1)
 	AutoInt.Screen.SetLineWidth(1.0)
 	AutoInt.Screen.DrawLine(0, float64((AutoInt.Display.Height-14)*-1), float64(AutoInt.Display.Width), float64((AutoInt.Display.Height-14)*-1))
 	AutoInt.Screen.Stroke()
