c21feae4565cd9606763fb133c49e431993ad658
Tyler Hardison <tyler@stninc.com>
2022-03-09 14:46:19 -0800

    ..

--- auto_interface/clock.go
+++ auto_interface/clock.go
@@ -14,7 +14,7 @@ func (AutoInt *AutoInterface) DrawClock() {
 	currentTime := time.Now()
 	time := currentTime.Format("Mon, Jan 2 15:04")
 	AutoInt.Screen.SetColor(color.Black)
-
+/* 
 	font, err := truetype.Parse(goregular.TTF)
 
 	if err != nil {
@@ -25,7 +25,7 @@ func (AutoInt *AutoInterface) DrawClock() {
 		Size: 10,
 	})
 	AutoInt.Screen.SetFontFace(face)
-
+ */
 	wtime, htime := AutoInt.Screen.MeasureString(time)
 
 	log.Printf("width %f, height %f", wtime, htime)
