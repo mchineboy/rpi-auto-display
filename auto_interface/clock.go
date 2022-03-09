2c5d05301df636c26ec57875e79ad5da1a8f730c
Tyler Hardison <tyler@stninc.com>
2022-03-09 15:17:44 -0800

    ..

--- auto_interface/clock.go
+++ auto_interface/clock.go
@@ -2,34 +2,29 @@ package auto_interface
 
 import (
 	"image/color"
-	"log"
 	"time"
 
 	"github.com/fogleman/gg"
+	"github.com/golang/freetype/truetype"
+	"golang.org/x/image/font/gofont/goregular"
 )
 
 func (AutoInt *AutoInterface) DrawClock() {
 	currentTime := time.Now()
 	time := currentTime.Format("Mon, Jan 2 15:04")
 	AutoInt.Screen.SetColor(color.Black)
-	/*
-		font, err := truetype.Parse(goregular.TTF)
+	font, err := truetype.Parse(goregular.TTF)
 
-		if err != nil {
-			panic(err)
-		}
+	if err != nil {
+		panic(err)
+	}
 
-		face := truetype.NewFace(font, &truetype.Options{
-			Size: 10,
-		})
-		AutoInt.Screen.SetFontFace(face)
-	*/
-	wtime, htime := AutoInt.Screen.MeasureString(time)
-
-	log.Printf("width %f, height %f", wtime, htime)
+	face := truetype.NewFace(font, &truetype.Options{
+		Size: 10,
+	})
+	AutoInt.Screen.SetFontFace(face)
+	_, htime := AutoInt.Screen.MeasureString(time)
 	AutoInt.Screen.Rotate(gg.Radians(90))
-	log.Printf("x: %f, y: %f\n", float64(AutoInt.Display.Height)-htime, float64((AutoInt.Display.Width-14)*-1))
 	AutoInt.Screen.DrawStringAnchored(time, (float64(AutoInt.Display.Height) - htime), float64((AutoInt.Display.Width)*-1), 1, 1)
 	AutoInt.Screen.Stroke()
-	log.Printf("Update clock: %s\n", time)
 }
