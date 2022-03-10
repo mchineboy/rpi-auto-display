6e6e88534f3af89e365ab76cc0723061d08db317
Tyler Hardison <tyler@stninc.com>
2022-03-09 16:01:08 -0800

    ..

--- auto_interface/drawx.go
+++ auto_interface/drawx.go
@@ -19,19 +19,15 @@ func (AutoInt *AutoInterface) DrawX() {
 	var hs2, ws2 = AutoInt.Screen.MeasureString(s2)
 
 	AutoInt.Screen.SetColor(color.Black)
-	AutoInt.Screen.DrawRectangle(cx-(hs2/2)-4, cy-(ws2/2)-6, hs2+8, ws2+6)
+	AutoInt.Screen.DrawRectangle(cx-(hs2/2)-4, (cy-(ws2/2)-6)*-1, hs2+8, (ws2+6)*-1)
 	AutoInt.Screen.Fill()
 
 	AutoInt.Screen.SetColor(color.Black)
-	AutoInt.Screen.DrawString(s1, cx-(hs1/2), cy-ws2-8)
+	AutoInt.Screen.DrawString(s1, cx-(hs1/2), (cy-ws2-8)*-1)
 	AutoInt.Screen.Stroke()
 
 	AutoInt.Screen.SetColor(color.White)
-	AutoInt.Screen.DrawString(s2, cx-(hs2/2), cy)
+	AutoInt.Screen.DrawString(s2, cx-(hs2/2), cy*-1)
 	AutoInt.Screen.Stroke()
 
-	if e := AutoInt.Display.Draw(AutoInt.Screen.Image()); e != nil {
-		log.Printf("[ERROR] failed to draw: %v\n", e)
-		AutoInt.Display.Clear(color.White)
-	}
 }
