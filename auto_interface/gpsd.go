2f00eb27606e034a96f06dfc21686601d6445c18
Tyler Hardison <tyler@stninc.com>
2022-03-10 07:41:29 -0800

    ..

--- auto_interface/gpsd.go
+++ auto_interface/gpsd.go
@@ -3,12 +3,14 @@ package auto_interface
 import (
 	"fmt"
 	"image/color"
+	"log"
 
 	"github.com/fogleman/gg"
 )
 
 func (AutoInt *AutoInterface) DrawGPS() {
 	gpsstring := fmt.Sprintf("Lat: %0.4f Lon: %0.4f Ele: %0.4f", AutoInt.Agps.TPV.Lat, AutoInt.Agps.TPV.Lon, AutoInt.Agps.TPV.Alt)
+	log.Printf("%s\n", gpsstring)
 	AutoInt.Screen.SetColor(color.Black)
 	AutoInt.Screen.Rotate(gg.Radians(90))
 	AutoInt.Screen.DrawStringAnchored(gpsstring, 0, -14, 0, 0)
