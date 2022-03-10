800a06b80ab338249a7346d22e9eb3e1511849a5
Tyler Hardison <tyler.hardison@voltacharging.com>
2022-03-10 11:04:16 -0800

    ..

--- auto_interface/gpsd.go
+++ auto_interface/gpsd.go
@@ -12,10 +12,10 @@ func (AutoInt *AutoInterface) DrawGPS() {
 	gpsstring := fmt.Sprintf("Lat: %0.6f Lon: %0.6f Ele: %0.0f'", AutoInt.Agps.TPV.Lat, AutoInt.Agps.TPV.Lon, AutoInt.Agps.TPV.Alt*3.281)
 	log.Printf("%s\n", gpsstring)
 	AutoInt.Screen.SetColor(color.Black)
-	AutoInt.Screen.Rotate(gg.Radians(90))
+	//AutoInt.Screen.Rotate(gg.Radians(90))
 	_, htime := AutoInt.Screen.MeasureString(gpsstring)
 	AutoInt.Screen.DrawStringAnchored(gpsstring, (float64(AutoInt.Display.Height) - htime), -14, .9, 1)
-	AutoInt.Screen.Stroke()
+	//AutoInt.Screen.Stroke()
 
 	AutoInt.Screen.Rotate(gg.Radians(0)) // Reset rotate once completed
 }
