27ae7e36d1e0b88317192036a9a4a45bfb714adb
Tyler Hardison <tyler@stninc.com>
2022-03-09 16:22:18 -0800

    ..

--- auto_interface/drawx.go
+++ auto_interface/drawx.go
@@ -11,6 +11,6 @@ func (AutoInt *AutoInterface) DrawX() {
 	AutoInt.Screen.SetLineCapButt()
 	AutoInt.Screen.DrawLine(0, (float64(AutoInt.Display.Width)*-1)-13, float64(AutoInt.Display.Height), (float64(AutoInt.Display.Width)*-1)-13)
 	//AutoInt.Screen.DrawLine(0.0, float64((AutoInt.Display.Width-14)*-1), float64(AutoInt.Display.Height), float64((AutoInt.Display.Width-14)*-1))
-	log.Printf("%f %f %f %f\n", 0.0, float64((AutoInt.Display.Width-14)*-1), float64(AutoInt.Display.Height), float64((AutoInt.Display.Width-14)*-1))
+	log.Printf("%f %f %f %f\n", 0.0, (float64(AutoInt.Display.Width)*-1)-13, float64(AutoInt.Display.Height), (float64(AutoInt.Display.Width)*-1)-13)
 	AutoInt.Screen.Stroke()
 }
