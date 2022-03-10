e1198e970a7b73769f9ae477d9f98b8d298a6da6
Tyler Hardison <tyler@stninc.com>
2022-03-09 16:50:23 -0800

    ..

--- auto_interface/drawx.go
+++ auto_interface/drawx.go
@@ -7,8 +7,7 @@ import (
 
 func (AutoInt *AutoInterface) DrawX() {
 	AutoInt.Screen.SetColor(color.Black)
-	AutoInt.Screen.SetLineWidth(1.5)
-	AutoInt.Screen.SetLineCapButt()
+	AutoInt.Screen.SetLineWidth(2.0)
 	AutoInt.Screen.DrawLine(0.0, 15-(float64(AutoInt.Display.Width)), float64(AutoInt.Display.Height), 15-(float64(AutoInt.Display.Width)))
 	//AutoInt.Screen.DrawLine(0.0, float64((AutoInt.Display.Width-14)*-1), float64(AutoInt.Display.Height), float64((AutoInt.Display.Width-14)*-1))
 	log.Printf("%f %f %f %f\n", 0.0, 15-(float64(AutoInt.Display.Width)), float64(AutoInt.Display.Height), 15-(float64(AutoInt.Display.Width)))
