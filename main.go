575ad6c5edff20f97bfff79482a6a35c9929dee5
Tyler Hardison <tyler.hardison@voltacharging.com>
2022-03-12 11:04:25 -0800

    ..

--- main.go
+++ main.go
@@ -1,11 +1,14 @@
 package main
 
 import (
+	"log"
+
 	"github.com/mchineboy/rpi-auto-display/auto_gps"
 	"github.com/mchineboy/rpi-auto-display/auto_interface"
 )
 
 func main() {
+	log.Printf("%s\n", "Hello World!")
 	Agps := auto_gps.New()
 	go func() { Agps.Monitor() }()
 	_ = auto_interface.New(Agps)
