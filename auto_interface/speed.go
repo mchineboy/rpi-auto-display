8e9c3ac3235e7068d809605cdc913f8841de2b3f
Tyler Hardison <tyler.hardison@voltacharging.com>
2022-03-23 10:47:17 -0700

    ..

--- auto_interface/speed.go
+++ auto_interface/speed.go
@@ -12,6 +12,6 @@ func (AutoInt *AutoInterface) DrawSpeed() {
 	AutoInt.Screen.SetFontFace(AutoInt.Fonts["race-36"])
 	log.Printf("%02d", int(AutoInt.Agps.TPV.Speed*1.609))
 	AutoInt.Screen.DrawStringAnchored(
-		fmt.Sprintf("%02d", int(AutoInt.Agps.TPV.Speed*1.609)), 0, (float64(AutoInt.Display.Width) * -1), 0.5, 0.5)
+		fmt.Sprintf("%02d", int(AutoInt.Agps.TPV.Speed*1.609)), 1, (float64(AutoInt.Display.Width) * -1), 0.0, 1.0)
 	AutoInt.Screen.Stroke()
 }
