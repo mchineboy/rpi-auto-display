36cd02d4ed279b3299a8fc10c677f3d5f4caddc2
Tyler Hardison <tyler.hardison@voltacharging.com>
2022-03-23 09:52:30 -0700

    ..

--- auto_interface/speed.go
+++ auto_interface/speed.go
@@ -3,12 +3,14 @@ package auto_interface
 import (
 	"fmt"
 	"image/color"
+	"log"
 )
 
 func (AutoInt *AutoInterface) DrawSpeed() {
 	AutoInt.Screen.SetColor(color.Black)
 
 	AutoInt.Screen.SetFontFace(AutoInt.Fonts["race-36"])
+	log.Printf("%02d", int(AutoInt.Agps.TPV.Speed*1.609))
 	AutoInt.Screen.DrawStringAnchored(
 		fmt.Sprintf("%02d", int(AutoInt.Agps.TPV.Speed*1.609)), 0, (float64(AutoInt.Display.Width) * -1), 0.5, 0.5)
 	AutoInt.Screen.Stroke()
