47533971c843f54de2edf13ec1f33aaddc5eb45c
Tyler Hardison <tyler.hardison@voltacharging.com>
2022-03-29 14:06:35 -0700

    maybe?

--- auto_interface/speed.go
+++ auto_interface/speed.go
@@ -12,7 +12,7 @@ func (AutoInt *AutoInterface) DrawSpeed() {
 	AutoInt.Screen.SetFontFace(AutoInt.Fonts["digital-96"])
 	log.Printf("%02d", int(AutoInt.Agps.TPV.Speed*1.609))
 	AutoInt.Screen.DrawStringAnchored(
-		fmt.Sprintf("%02d", int(AutoInt.Agps.TPV.Speed*1.609)), 8, (float64(AutoInt.Display.Width-18) * -1), 0.0, 1.0)
+		fmt.Sprintf("%02d", int(AutoInt.Agps.TPV.Speed*1.609)), 8, (float64(AutoInt.Display.Width-7) * -1), 0.0, 1.0)
 	AutoInt.Screen.Stroke()
 
 	AutoInt.Screen.SetFontFace(AutoInt.Fonts["race-18"])
