8f4d1bc24d1400a7dffc30ec4c377169ca411981
Tyler Hardison <tyler.hardison@voltacharging.com>
2022-03-29 13:17:53 -0700

    ..

--- auto_interface/speed.go
+++ auto_interface/speed.go
@@ -15,10 +15,10 @@ func (AutoInt *AutoInterface) DrawSpeed() {
 		fmt.Sprintf("%02d", int(AutoInt.Agps.TPV.Speed*1.609)), 1, (float64(AutoInt.Display.Width-14) * -1), 0.0, 1.0)
 	AutoInt.Screen.Stroke()
 
-	AutoInt.Screen.SetFontFace(AutoInt.Fonts["default-13"])
+	AutoInt.Screen.SetFontFace(AutoInt.Fonts["race-16"])
 	compass := AutoInt.Agps.GetCompassDirection(AutoInt.Agps.TPV.Track)
 	AutoInt.Screen.DrawStringAnchored(
-		fmt.Sprintf("%03s", compass), 0, -15, 0, 0.0,
+		fmt.Sprintf("%6s", compass), 0, -18, 0, 0.0,
 	)
 	AutoInt.Screen.Stroke()
 }
